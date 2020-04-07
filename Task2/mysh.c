749beb1f10a0a7bf9494629735eb4b4c0a38a869
zhuo1ang <zhuo1angT@outlook.com>
2020-04-07 22:09:16 +0800

    update

--- Task2/mysh.c
+++ Task2/mysh.c
@@ -3,25 +3,32 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
-#define MAXLINE 500
+#define MAX_LINE 500
+#define MAX_ARGS 50
 
-void eval(char *cmd);
+void eval(char *cmdline);
 void sigint_handler(int sig);
 
 char *cmdline;
-int fg = false, fg_pid;
+char *argc[MAX_ARGS], *envp[MAX_ARGS];
+char cmd_buffer[MAX_LINE];
+
+int fg = false, fg_pid, sh_pid;
 
 int main() {
-    cmdline = (char *)malloc(sizeof(char) * MAXLINE);
-    signal(SIGINT, sigint_handler);
+    signal(SIGINT, sigint_handler);  // installing the "ctrl+C" handler
+
+    sh_pid = getpid();
 
     printf("my-shell.\n");
 
     while (true) {
         cmdline = readline("\e[36;1mzhuo1ang@my-shell > \e[0m");
+
         if (cmdline == NULL) {
             write(1, "\n", 1);  // stdout <- "\n"
             exit(0);
@@ -29,29 +36,30 @@ int main() {
 
         // temp code start
 
-        char *argv[2];
+        char *argv[2] = {};
         argv[0] = cmdline;
         argv[1] = NULL;
         if ((fg_pid = fork()) == 0) {
             execve(cmdline, argv, NULL);
         } else {
             fg = true;
-            wait(NULL);
+            printf("pid = %d\n", fg_pid);
+            waitpid(fg_pid, NULL, 0);
         }
 
         // temp code end
 
         // eval(cmdline);
+
+        free(cmdline);
     }
 
     return 0;
 }
 
-void eval(char *cmd) {}
+void eval(char *cmdline) {}
 void sigint_handler(int sig) {
-    if (fg) {
-        kill(fg_pid, SIGKILL);
-        waitpid(fg_pid, NULL, 0);
-        fg = false;
-    }
+    write(1, "\n", 1);  // stdout <- "\n"
+    kill(fg_pid, 0);    // kill(fg_pid, 0)     // to fix
+    fg = false;
 }
