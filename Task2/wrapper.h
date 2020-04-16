c158573057a347fe474575148ec83da8c72e49e5
zhuo1ang <zhuo1angT@Outlook.com>
2020-04-16 15:12:34 +0800

    update readline history

--- Task2/wrapper.h
+++ Task2/wrapper.h
@@ -11,49 +11,49 @@
 #include <unistd.h>
 
 void unix_error(char *msg) {  // unix-style error
-  fprintf(stderr, "%s\nerrno:\"%s\"\n", msg, strerror(errno));
-  exit(0);
+    fprintf(stderr, "%s\nerrno:\"%s\"\n", msg, strerror(errno));
+    exit(0);
 }
 
 pid_t Fork() {
-  pid_t pid;
-  if ((pid = fork()) < 0) unix_error("Error: fork failed!");
-  return pid;
+    pid_t pid;
+    if ((pid = fork()) < 0) unix_error("Error: fork failed!");
+    return pid;
 }
 
 pid_t Wait(int *status) {
-  pid_t pid;
-  if ((pid = wait(status)) < 0) unix_error("Error: wait failed!");
-  return pid;
+    pid_t pid;
+    if ((pid = wait(status)) < 0) unix_error("Error: wait failed!");
+    return pid;
 }
 
 pid_t Waitpid(pid_t pid, int *status, int options) {
-  pid_t ret;
-  if ((ret = waitpid(pid, status, options)) < 0)
-    unix_error("Error: waitpid failed!");
-  return ret;
+    pid_t ret;
+    if ((ret = waitpid(pid, status, options)) < 0)
+        unix_error("Error: waitpid failed!");
+    return ret;
 }
 
 int Execve(char *filename, char *argv[], char *envp[]) {
-  int ret;
-  if ((ret = execve(filename, argv, envp)) < 0)
-    unix_error("Error: execve failed!");
-  return ret;
+    int ret;
+    if ((ret = execve(filename, argv, envp)) < 0)
+        unix_error("Error: execve failed!");
+    return ret;
 }
 
 int Kill(pid_t pid, int sig) {
-  int ret;
-  if ((ret == kill(pid, sig)) < 0) {
-    unix_error("Error: kill failed!");
-  }
-  return ret;
+    int ret;
+    if ((ret == kill(pid, sig)) < 0) {
+        unix_error("Error: kill failed!");
+    }
+    return ret;
 }
 
 int Pipe(int pipefd[2]) {
-  int ret;
-  if ((ret == pipe(pipefd)) < 0) {
-    unix_error("Error: pipe failed!");
-  }
-  return ret;
+    int ret;
+    if ((ret == pipe(pipefd)) < 0) {
+        unix_error("Error: pipe failed!");
+    }
+    return ret;
 }
 #endif
