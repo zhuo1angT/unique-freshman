c2412313c7e3aeb40d5bd341d3b1837dd0a82ccc
zhuo1ang <zhuo1angT@Outlook.com>
2020-05-12 12:10:27 +0800

    update

--- Task4/mysocks_client.py
+++ Task4/mysocks_client.py
@@ -18,18 +18,58 @@ s1.close()
 
 import socket
 import base64
+import getopt
+import sys
 
 from utils import *
+from proxy import *
 
 
-# Get an socket bind to "address", and listening to at most "max_thread" connections
-def socks_init(address: tuple, max_thread: int) -> socket.socket:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(address)
-    s.listen(max_thread)
-    return s
+def main(argv):
+
+    server = ThreadingTCPServer(LOCAL_ADDR, Handler)
+    print('Debug:: listening')
+    server.serve_forever()  # 监听，建立好TCP连接后，为该连接创建新的socket和线程，并由处理类中的handle方法处理
+    print(server)
+
+    try:
+        opts, args = getopt.getopt(
+            argv, "s:p:b:l:k:", ["server_addr=", "server_port=", "local_addr=", "local_port=", "password="])
+
+    except getopt.GetoptError:
+        print("Error: mysocks_client.py, unrecognized argument")
+        sys.exit(2)
+
+    #  -s SERVER_ADDR  server address
+    #  -p SERVER_PORT  server port, default: 8388
+    #  -b LOCAL_ADDR   local binding address, default: 127.0.0.1
+    #  -l LOCAL_PORT   local port, default: 1080
+    #  -k PASSWORD     password
+    # 处理 返回值options是以元组为元素的列表。
+
+    server_addr = ""
+    server_port = -1
+    local_addr = ""
+    local_port = -1
+    password = ""
+
+    for opt, arg in opts:
+        if opt in ("-s", "--server_addr"):
+            NotImplemented
+        elif opt in ("-p", "--server_port"):
+            NotImplemented
+        elif opt in ("-b", "--local_addr"):
+            NotImplemented
+        elif opt in ("-l", "--local_port"):
+            NotImplemented
+        elif opt in ("-k", "--password"):
+            NotImplemented
+
+    # 打印 返回值args列表，即其中的元素是那些不含'-'或'--'的参数。
+    for i in range(0, len(args)):
+        print('参数 %s 为：%s' % (i + 1, args[i]))
 
 
-def socks_send(socket: socket.socket, address: tuple, message: bytes):
-    # s.send()
-    return 0
+if __name__ == '__main__':
+
+    main(sys.argv[1:])
--- Task4/proxy.py
+++ Task4/proxy.py
@@ -1,12 +1,15 @@
 import socket
 import base64
+import threading
 
+from socketserver import BaseRequestHandler, ThreadingTCPServer
 
 from utils import *
 
-
-LOCAL_ADDR = ("127.0.0.1", 1080)
+LOCAL_ADDR = ("127.0.0.1", 1081)
 SERVER_ADDR = ("", 0)
+
+BUF_SIZE = 4 * 1024
 
 
 class proxy_client:
@@ -15,8 +18,14 @@ class proxy_client:
     MAX_THREAD = 255
     METHODS = [0, 2]  # NO AUTHENTICATION REQUIRED & USERNAME / PASSWORD
 
+    INITIALIZED = 0
+    CONNECTED = 1
+    NEGOTIATED = 2
+    VERIFIED = 3
+
     def init(self, remote_addr: tuple, key: str, local_addr: tuple, encrypt: int,
              name="default", timeout=10) -> None:
+
         self.name = name
         self.remote_addr = remote_addr
         self.key = key
@@ -24,12 +33,15 @@ class proxy_client:
         self.encrypt = encrypt
         self.timeout = timeout
 
+        self.status = proxy_client.INITIALIZED
+
     def connect(self) -> None:
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind(self.local_addr)
         self.socket.listen(proxy_client.MAX_THREAD)
 
         self.socket.connect(self.remote_addr)
+        self.status = proxy_client.CONNECTED
 
     # |VER | NMETHODS | METHODS  |
     # +----+----------+----------+
@@ -45,6 +57,8 @@ class proxy_client:
             packet_str += to_hex(method, 2)  # METHODS
 
         self.socket.send(base64.b16decode(packet_str, True))
+
+        self.status = proxy_client.NEGOTIATED
 
     # |VER | NUSERNAME | USERNAME | NPASSWORD | PASSWORD |
     # +----+-----------+----------+-----------|----------|
@@ -62,6 +76,8 @@ class proxy_client:
 
         self.socket.send(base64.b16decode(packet_str, True))
 
+        self.status = proxy_client.VERIFIED
+
     # +----+-----+-------+------+----------+----------+
     # |VER | CMD |  RSV  | ATYP | DST.ADDR | DST.PORT |
     # +----+-----+-------+------+----------+----------+
@@ -72,7 +88,7 @@ class proxy_client:
 
     def socks_requst(self, address: tuple, cmd: int) -> None:
 
-        atyp = 1
+        atyp = 1  # ipv4
 
         packet_str = ""
 
@@ -84,3 +100,25 @@ class proxy_client:
         packet_str += to_hex(address[1], 4)  # target port
 
         self.socket.send(base64.b16decode(packet_str, True))
+
+    def socks_send(self, address: tuple, data: bytes) -> None:
+
+        self.socket.send(data)
+        self.socket.recv(BUF_SIZE)  # 4 KB
+
+
+class Handler(BaseRequestHandler):
+    def handle(self):
+        address, pid = self.client_address
+        print('%s connected!' % address)
+        while True:
+            data = self.request.recv(BUF_SIZE)
+            if len(data) > 0:
+                print('receive=', data.decode('utf-8'))
+                # cur_thread = threading.current_thread()
+                # response = '{}:{}'.format(cur_thread.ident,data)
+                self.request.sendall('response'.encode('utf-8'))
+                print('send:', 'response')
+            else:
+                print('close')
+                break
