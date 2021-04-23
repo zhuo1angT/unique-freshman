c193ad18ddd42d6320dac3d90e69c2a496854baf
Zhuoran He <zhuo1angT@Outlook.com>
2021-04-23 16:01:00 +0800

    refactor

--- Task2/builtin.h
+++ Task2/builtin.h
@@ -10,81 +10,83 @@
 
 #include "wrapper.h"
 
-#define MAX_LINE 500
-#define MAX_ARGS 50
+#define MAX_LINE 1024
+#define MAX_ARGS 256
 
 char echo_buf[MAX_LINE];
 char pwd_buf[MAX_LINE];
 
 extern int argc;
-extern char* argv[];
+extern char *argv[];
 extern int input_fd, output_fd;
 
 extern FILE *input_file, *output_file;
 
 bool is_builtin(char cmd[]);
-void exec_builtin(char cmd[], char* argv[], char* envp);
+void exec_builtin(char cmd[], char *argv[], char *envp);
 
-void myEcho(char* argv[]) {
-    for (int i = 1; i < argc; i++) {
-        write(output_fd, argv[i], strlen(argv[i]));
-        write(output_fd, " ", 1);
-    }
-    write(output_fd, "\n", 1);
+void myEcho(char *argv[]) {
+  for (int i = 1; i < argc; i++) {
+    write(output_fd, argv[i], strlen(argv[i]));
+    write(output_fd, " ", 1);
+  }
+  write(output_fd, "\n", 1);
 }
 
 void myExit() { exit(0); }
 
-void myCd(const char* target_dir) {
-    if (chdir(target_dir) == -1) {
-        unix_error("Error: cd failed\n");
-    }
+void myCd(const char *target_dir) {
+  if (chdir(target_dir) == -1) {
+    unix_error("Error: cd failed\n");
+  }
 }
 
 void myPwd() {
-    if (getcwd(pwd_buf, MAX_LINE) == NULL) {
-        unix_error("Error: pwd failed\n");
-    }
-    if (output_fd == 1)
-        printf("%s\n", pwd_buf);
-    else
-        write(output_fd, pwd_buf, strlen(pwd_buf));
+  if (getcwd(pwd_buf, MAX_LINE) == NULL) {
+    unix_error("Error: pwd failed\n");
+  }
+  if (output_fd == 1)
+    printf("%s\n", pwd_buf);
+  else
+    write(output_fd, pwd_buf, strlen(pwd_buf));
 }
 
 void myKill(int pid) {
-    if (pid != 0) Kill(pid, SIGTERM);
+  if (pid != 0)
+    Kill(pid, SIGTERM);
 }
 
-void myExport(char* argv[]) {
-    char *ptr0 = argv[1], *ptr1 = argv[1];
-    while (*ptr1 != '=') ptr1++;
-    *ptr1 = '\0';
-    setenv(ptr0, ptr1, 1);
+void myExport(char *argv[]) {
+  char *ptr0 = argv[1], *ptr1 = argv[1];
+  while (*ptr1 != '=')
+    ptr1++;
+  *ptr1 = '\0';
+  setenv(ptr0, ptr1, 1);
 }
 
 bool is_builtin(char cmd[]) {
-    if (strcmp(cmd, "echo") == 0 || strcmp(cmd, "exit") == 0 ||
-        strcmp(cmd, "cd") == 0 || strcmp(cmd, "pwd") == 0 ||
-        strcmp(cmd, "kill") == 0 || strcmp(cmd, "export") == 0) {
-        return true;
-    } else
-        return false;
+  if (strcmp(cmd, "echo") == 0 || strcmp(cmd, "exit") == 0 ||
+      strcmp(cmd, "cd") == 0 || strcmp(cmd, "pwd") == 0 ||
+      strcmp(cmd, "kill") == 0 || strcmp(cmd, "export") == 0) {
+    return true;
+  } else
+    return false;
 }
 
-void exec_builtin(char cmd[], char* argv[], char* envp) {
-    if (strcmp(cmd, "echo") == 0) {
-        myEcho(argv);
-    } else if (strcmp(cmd, "exit") == 0) {
-        myExit();
-    } else if (strcmp(cmd, "cd") == 0) {
-        myCd(argv[1]);
-    } else if (strcmp(cmd, "pwd") == 0) {
-        myPwd();
-    } else if (strcmp(cmd, "kill") == 0) {
-        myKill(atoi(argv[1]));
-    } else if (strcmp(cmd, "export") == 0) {
-        myExport(argv);
-    }
+void exec_builtin(char cmd[], char *argv[], char *envp) {
+  if (strcmp(cmd, "echo") == 0) {
+    myEcho(argv);
+  } else if (strcmp(cmd, "exit") == 0) {
+    myExit();
+  } else if (strcmp(cmd, "cd") == 0) {
+    myCd(argv[1]);
+  } else if (strcmp(cmd, "pwd") == 0) {
+    myPwd();
+  } else if (strcmp(cmd, "kill") == 0) {
+    myKill(atoi(argv[1]));
+  } else if (strcmp(cmd, "export") == 0) {
+    myExport(argv);
+  }
 }
 
 /*
--- Task2/mysh.c
+++ Task2/mysh.c
@@ -12,15 +12,13 @@
 
 #include "builtin.h"
 
-#define MAX_LINE 1024
-#define MAX_ARGS 256
 #define MAX_ARG_LENGTH 64
 #define MAX_ENVP_LENGTH 64
 
 char *cmdline;
 
 int argc;
-char *argv[MAX_ARGS], *envp[MAX_ARGS];
+char *argv[MAX_ARGS], *envp[MAX_ENVP_LENGTH];
 char cmd_buffer[MAX_LINE];
 
 char prompt[MAX_LINE];
@@ -32,7 +30,7 @@ int pipe_fd[2];
 char pipe_buf[4 * 1024];
 
 int rplc_stdin = false, rplc_stdout = false, rplc_stderr = false;
-int save_stdin, save_stdout, save_stderr;
+int saved_stdin, saved_stdout, save_stderr;
 
 void parse(char *cmdline);
 void set_bg();
@@ -73,12 +71,11 @@ int main() {
 
     if (!set_pipe()) {
     exec:
-
       if (is_builtin(argv[0])) {
         exec_builtin(argv[0], argv, NULL);
       } else {
         if ((pid = fork()) == 0) {  // child process
-          execve(argv[0], argv, NULL);
+          Execve(argv[0], argv, envp);
           exit(0);
         }
 
@@ -167,54 +164,42 @@ void set_bg() {
 int io_redirect() {
   for (int i = 0; i < argc; i++) {
     if (strcmp(argv[i], "<") == 0) {
-      if (i + 2 == argc ||
-          (i + 3 == argc && strcmp(argv[argc - 1], "&") == 0)) {
-        if (!is_builtin(argv[0])) {
-          save_stdin = dup(0);
-          FILE *new_input_stream = freopen(argv[i + 1], "r", stdin);
-        } else {
-          input_file = fopen(argv[i + 1], "r");
-          input_fd = input_file->_fileno;
-        }
-        rplc_stdin = true;
-        argv[i] = NULL;
-        argc = i;
-        return true;
-
+      if (!is_builtin(argv[0])) {
+        saved_stdin = dup(0);
+        FILE *new_input_stream = freopen(argv[i + 1], "r", stdin);
       } else {
-        // Todo: Error report
+        input_file = fopen(argv[i + 1], "r");
+        input_fd = input_file->_fileno;
       }
+      rplc_stdin = true;
+      argv[i] = NULL;
+      argc = i;
+      return 1;
     }
     if (strcmp(argv[i], ">") == 0) {
-      if (i + 2 == argc ||
-          (i + 3 == argc && strcmp(argv[argc - 1], "&") == 0)) {
-        if (!is_builtin(argv[0])) {
-          save_stdout = dup(1);
-          FILE *new_output_stream = freopen(argv[i + 1], "w", stdout);
-        } else {
-          output_file = fopen(argv[i + 1], "w");
-          output_fd = output_file->_fileno;
-        }
-        rplc_stdout = true;
-        argv[i] = NULL;
-        argc = i;
-        return true;
-
+      if (!is_builtin(argv[0])) {
+        saved_stdout = dup(1);
+        FILE *new_output_stream = freopen(argv[i + 1], "w", stdout);
       } else {
-        // Todo: Error report
+        output_file = fopen(argv[i + 1], "w");
+        output_fd = output_file->_fileno;
       }
+      rplc_stdout = true;
+      argv[i] = NULL;
+      argc = i;
+      return 1;
     }
   }
-  return false;
+  return 0;
 }
 
 void io_reset() {
   if (rplc_stdin) {
-    dup2(save_stdin, 0);
+    dup2(saved_stdin, 0);
     rplc_stdin = false;
   }
   if (rplc_stdout) {
-    dup2(save_stdout, 1);
+    dup2(saved_stdout, 1);
     rplc_stdout = false;
   }
   if (rplc_stderr) {
@@ -229,7 +214,7 @@ void io_reset() {
 }
 
 int set_pipe() {
-  // only one pipe
+  // support only one pipe
   for (int i = 0; i < argc; i++) {
     if (strcmp(argv[i], "|") == 0) {
       int argc1;
@@ -243,54 +228,34 @@ int set_pipe() {
         argv[i] = NULL;
         if (!is_builtin(argv[0])) {
           dup2(pipe_fd[1], 1);
-
-          execve(argv[0], argv, NULL);
+          Execve(argv[0], argv, envp);
         } else {
           output_fd = pipe_fd[1];
-
           exec_builtin(argv[0], argv, NULL);
           exit(0);
         }
       }
       if ((pid1 = Fork()) == 0) {
         close(pipe_fd[1]);  // close write
-
         for (int j = i + 1; j < argc; j++) {
           argv1[j - i - 1] = argv[j];
         }
         argv1[argc - i - 1] = NULL;
-
-        // dup2(pipe_fd[0], 0);
-
-        // temp code start
-        int _size_read = read(pipe_fd[0], pipe_buf, 4 * 1024);
-        FILE *pipe_file = fopen("pipe_buf_file", "w");
-
-        for (int i = 0; i < _size_read; i++)
-          fprintf(pipe_file, "%c", pipe_buf[i]);
-
-        fclose(pipe_file);
-
-        pipe_file = fopen("pipe_buf_file", "r");
-        // temp code end
-
         if (!is_builtin(argv1[0])) {
-          dup2(pipe_file->_fileno, 0);
-          execve(argv1[0], argv1, NULL);
+          dup2(pipe_fd[0], 0);
+          Execve(argv1[0], argv1, envp);
         } else {
           input_fd = pipe_fd[0];
           exec_builtin(argv[0], argv, NULL);
           exit(0);
         }
       }
+      close(pipe_fd[0]);
+      close(pipe_fd[1]);
 
       int status;
-
       Waitpid(pid0, &status, 0);
-
       Waitpid(pid1, &status, 0);
-
-      remove("pipe_buf_file");
 
       return 1;
     }
